11bf6206600ef19823107712bb21f7fde7c39cb6
Tony <hpm3ep@virginia.edu>
2025-05-27 23:38:18 -0400

    fix import

--- backend/api_tools/team_details.py
+++ backend/api_tools/team_details.py
@@ -22,7 +22,7 @@ from functools import lru_cache
 from nba_api.stats.endpoints import teamdetails
 import pandas as pd
 
-from utils.path_utils import get_cache_dir, get_cache_file_path
+from ..utils.path_utils import get_cache_dir, get_cache_file_path
 
 # Define utility functions here since we can't import from .utils
 def _process_dataframe(df, single_row=False):
